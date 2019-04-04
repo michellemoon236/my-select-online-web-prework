cd22c289837356df9603265835341fc622f3aa8d
Michelle M <michellemoon236@gmail.com>
2019-04-04 23:54:17 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
- # your code here!
+i = 0 
+new_collection = []
+while i < collection.length 
+   if collection[i] % 2 == 0
+      new_collection << (collection[i])
+      i +=1 
+   else
+      puts collection[i]
+      i+=1
+end
+end
+new_collection
 end
